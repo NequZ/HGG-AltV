9fcab61e9fd2ba30feca4f4e6654b6c72948eff0
HighgroundGaming-DE <56088506+HighgroundGaming-DE@users.noreply.github.com>
2019-10-14 09:35:08 +0200

    remove licenses

--- server/server.mjs
+++ server/server.mjs
@@ -2,20 +2,3 @@ import * as TermsAndConditions from './configuration/terms-and-conditions.mjs'; 
 
 import fs from 'fs';
 
-if (!fs.existsSync('./resources/orp/terms-and-conditions.json')) {
-    fs.writeFileSync(
-        './resources/orp/terms-and-conditions.json',
-        JSON.stringify(TermsAndConditions.data, '', '\t')
-    );
-    throw new Error(
-        'Please read the terms and conditions and modify accordingly under terms-and-conditions.json'
-    );
-} else {
-    const data = fs.readFileSync('./resources/orp/terms-and-conditions.json').toString();
-    const parsed = JSON.parse(data);
-    if (!parsed.do_you_agree)
-        throw new Error(
-            'Please read the terms and conditions and modify accordingly under terms-and-conditions.json'
-        );
-    import('./startup.mjs');
-}
